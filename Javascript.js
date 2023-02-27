484fbbb5883012a877e7f9da98d264203bd497a1
Morittz <126390150+Morittz@users.noreply.github.com>
2023-02-27 10:18:11 +0100

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,4 +1,4 @@
-var haarfarbe, bereich, jahre, form, upload, optional, thanks;
+var haarfarbe, bereich, jahre, client_name, form, number, upload, optional, thanks;
 
 
 var we_tabs_next_button = '[bloc=next-question]';
@@ -194,8 +194,9 @@ var we_tabs_next_button = '[bloc=next-question]';
       $($('[data-w-tab=form]')).attr('prev-tab',we_activeTab);
 
       });
-    $('[bloc=next-question]').on('click',function() {
-  if($('.w--tab-active').attr('data-w-tab')=='form') {
+    $("[name=client_name]").on("input", function () {
+    client_name = getValueFromInput("client_name");
+      if (client_name == null && number == null) {
 
         nextTab = 'upload';
         we_activeTab = $(".w--tab-active").attr("data-w-tab");
@@ -214,9 +215,82 @@ var we_tabs_next_button = '[bloc=next-question]';
         $(".w--tab-active").attr('next-tab',nextTab);
         $($('[data-w-tab=upload]')).attr('prev-tab',we_activeTab);
 
+        }
+});
 
-      }});
-$('[bloc=next-question]').on('click',function() {
+    $("[name=client_name]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      client_name = getValueFromInput("client_name");
+        if (client_name == null && number == null) {
+
+        nextTab = 'upload';
+        we_activeTab = $(".w--tab-active").attr("data-w-tab");
+        we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+        we_indexOfNextTab = we_indexOfActiveTab + 1;
+        we_indexOfPrevTab = we_indexOfActiveTab - 1;
+        we_prevTab = tabList[we_indexOfPrevTab];
+        we_amountOfTabs = tabList.length;
+
+        if (we_indexOfNextTab < we_amountOfTabs) {
+          tabList[we_indexOfNextTab] = nextTab;
+        } else {
+          tabList.push(nextTab);
+        }
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+        $(".w--tab-active").attr('next-tab',nextTab);
+        $($('[data-w-tab=upload]')).attr('prev-tab',we_activeTab);
+
+        }
+});
+    $("[name=number]").on("input", function () {
+    number = getValueFromInput("number");
+      if (client_name == null && number == null) {
+
+        nextTab = 'upload';
+        we_activeTab = $(".w--tab-active").attr("data-w-tab");
+        we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+        we_indexOfNextTab = we_indexOfActiveTab + 1;
+        we_indexOfPrevTab = we_indexOfActiveTab - 1;
+        we_prevTab = tabList[we_indexOfPrevTab];
+        we_amountOfTabs = tabList.length;
+
+        if (we_indexOfNextTab < we_amountOfTabs) {
+          tabList[we_indexOfNextTab] = nextTab;
+        } else {
+          tabList.push(nextTab);
+        }
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+        $(".w--tab-active").attr('next-tab',nextTab);
+        $($('[data-w-tab=upload]')).attr('prev-tab',we_activeTab);
+
+        }
+});
+
+    $("[name=number]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      number = getValueFromInput("number");
+        if (client_name == null && number == null) {
+
+        nextTab = 'upload';
+        we_activeTab = $(".w--tab-active").attr("data-w-tab");
+        we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+        we_indexOfNextTab = we_indexOfActiveTab + 1;
+        we_indexOfPrevTab = we_indexOfActiveTab - 1;
+        we_prevTab = tabList[we_indexOfPrevTab];
+        we_amountOfTabs = tabList.length;
+
+        if (we_indexOfNextTab < we_amountOfTabs) {
+          tabList[we_indexOfNextTab] = nextTab;
+        } else {
+          tabList.push(nextTab);
+        }
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+        $(".w--tab-active").attr('next-tab',nextTab);
+        $($('[data-w-tab=upload]')).attr('prev-tab',we_activeTab);
+
+        }
+});
+    $('[bloc=next-question]').on('click',function() {
   if($('.w--tab-active').attr('data-w-tab')=='upload') {
 
         nextTab = 'optional';
