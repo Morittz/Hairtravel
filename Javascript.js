ed32c01fd9ac3b5acaab208d252831c4fd9f246d
Morittz <126390150+Morittz@users.noreply.github.com>
2023-02-27 11:31:08 +0100

    Update Javascript.js

--- Javascript.js
+++ Javascript.js
@@ -1,4 +1,4 @@
-var haarfarbe, bereich, jahre, form, upload;
+var haarfarbe, bereich, jahre, kundenname, form, upload, optional, thanks;
 
 
 var we_tabs_next_button = '[bloc=next-question]';
@@ -194,10 +194,54 @@ var we_tabs_next_button = '[bloc=next-question]';
       $($('[data-w-tab=form]')).attr('prev-tab',we_activeTab);
 
       });
-    $('[bloc=datenschutz]').on('click',function() {
-  if($('.w--tab-active').attr('data-w-tab')=='form') {
+    $("[name=kundenname]").on("input", function () {
+    kundenname = getValueFromInput("kundenname");
 
-        nextTab = 'upload';
+      nextTab = 'upload';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=upload]')).attr('prev-tab',we_activeTab);
+
+      });
+
+    $("[name=kundenname]").parent("label.w-radio").on("click", function () {
+      clickedRadioButtonValue = $("input", this).val();
+      kundenname = getValueFromInput("kundenname");
+
+      nextTab = 'upload';
+      we_activeTab = $(".w--tab-active").attr("data-w-tab");
+      we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+      we_indexOfNextTab = we_indexOfActiveTab + 1;
+      we_indexOfPrevTab = we_indexOfActiveTab - 1;
+      we_prevTab = tabList[we_indexOfPrevTab];
+      we_amountOfTabs = tabList.length;
+
+      if (we_indexOfNextTab < we_amountOfTabs) {
+        tabList[we_indexOfNextTab] = nextTab;
+      } else {
+        tabList.push(nextTab);
+      }
+      $(we_tabs_next_button).addClass(we_tabs_active_class);
+      $(".w--tab-active").attr('next-tab',nextTab);
+      $($('[data-w-tab=upload]')).attr('prev-tab',we_activeTab);
+
+      });
+    $('[bloc=next-question]').on('click',function() {
+  if($('.w--tab-active').attr('data-w-tab')=='upload') {
+
+        nextTab = 'optional';
         we_activeTab = $(".w--tab-active").attr("data-w-tab");
         we_indexOfActiveTab = tabList.indexOf(we_activeTab);
         we_indexOfNextTab = we_indexOfActiveTab + 1;
@@ -212,7 +256,29 @@ var we_tabs_next_button = '[bloc=next-question]';
         }
         $(we_tabs_next_button).addClass(we_tabs_active_class);
         $(".w--tab-active").attr('next-tab',nextTab);
-        $($('[data-w-tab=upload]')).attr('prev-tab',we_activeTab);
+        $($('[data-w-tab=optional]')).attr('prev-tab',we_activeTab);
+
+
+      }});
+$('[bloc=next-question]').on('click',function() {
+  if($('.w--tab-active').attr('data-w-tab')=='optional') {
+
+        nextTab = 'thanks';
+        we_activeTab = $(".w--tab-active").attr("data-w-tab");
+        we_indexOfActiveTab = tabList.indexOf(we_activeTab);
+        we_indexOfNextTab = we_indexOfActiveTab + 1;
+        we_indexOfPrevTab = we_indexOfActiveTab - 1;
+        we_prevTab = tabList[we_indexOfPrevTab];
+        we_amountOfTabs = tabList.length;
+
+        if (we_indexOfNextTab < we_amountOfTabs) {
+          tabList[we_indexOfNextTab] = nextTab;
+        } else {
+          tabList.push(nextTab);
+        }
+        $(we_tabs_next_button).addClass(we_tabs_active_class);
+        $(".w--tab-active").attr('next-tab',nextTab);
+        $($('[data-w-tab=thanks]')).attr('prev-tab',we_activeTab);
 
 
       }});
